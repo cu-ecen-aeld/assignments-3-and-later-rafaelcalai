c65b2e8f868c3d0eabfd07f23577c43ee5f9d044
Rafael Calai <rafaelcalai@hotmail.com>
2024-12-29 17:18:16 +0000

    Implemented assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "## Deep clean kernel ${KERNEL_VERSION}"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+
+    echo "## Config using defconfig"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+
+    echo "## Compile kernel ${KERNEL_VERSION}"
+    make -j8 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+
+    echo "## Compile modules"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+
+    echo "## Compile dts"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +62,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo "## Create rootfs"
+ROOTFSDIR="$OUTDIR/rootfs"
+mkdir -p rootfs 
+cd rootfs
+
+mkdir -p bin sbin dev etc home lib lib64 proc sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +78,58 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    echo "## Config busybox"
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+echo "## Compile and install busybox"
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+make CONFIG_PREFIX=$ROOTFSDIR ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
+cd "$ROOTFSDIR"
+
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "Add dependencies"
+TOOLCHAINLIB64="/home/rcala/arm_toolchain/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64"
+cp $TOOLCHAINLIB64/libm.so.6 $ROOTFSDIR/lib64
+cp $TOOLCHAINLIB64/libresolv.so.2 $ROOTFSDIR/lib64
+cp $TOOLCHAINLIB64/libc.so.6 $ROOTFSDIR/lib64
+
+TOOLCHAINLIBC="/home/rcala/arm_toolchain/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib"
+cp $TOOLCHAINLIBC/ld-linux-aarch64.so.1 $ROOTFSDIR/lib64
 
 # TODO: Make device nodes
+echo "Make nodes"
+cd "$ROOTFSDIR"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+echo "Compile writer"
+cd $FINDER_APP_DIR
+make clean
+make 
 
 # TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+echo "Copy scripts to rootfs"
+cp -r finder.sh writer finder-test.sh conf autorun-qemu.sh "$ROOTFSDIR"/home/
 
+# on the target rootfs
+cd "$ROOTFSDIR"
 # TODO: Chown the root directory
+echo "hown the root directory"
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+
 
 # TODO: Create initramfs.cpio.gz
+echo "Create initramfs.cpio.gz"
+cd "$OUTDIR"
+gzip -f initramfs.cpio
