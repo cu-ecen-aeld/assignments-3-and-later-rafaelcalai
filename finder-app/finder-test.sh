95297b8e5e20ea154c2d3f683a2945a6ea4287d7
Rafael Calai <rafaelcalai@hotmail.com>
2025-01-02 15:31:58 +0000

    improve finder test script

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -2,13 +2,25 @@
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
 
+RUNNING_PATH=$(dirname $0)
+echo $RUNNING_PATH
+
+if [ $RUNNING_PATH == "/usr/bin" ]
+then
+	CONF_PATH="/etc/finder-app/conf"
+else
+	CONF_PATH="conf"
+fi
+
+echo "$RUNNING_PATH"
+
 set -e
 set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat $CONF_PATH/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +44,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat $CONF_PATH/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -54,10 +66,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	$RUNNING_PATH/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$($RUNNING_PATH/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
